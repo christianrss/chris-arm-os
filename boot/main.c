191ed475697892bc273537c11d1249073bdb1e27
Christian Rafael <christianparadix@gmail.com>
2026-04-04 14:10:17 -0300

    feat: Print function

--- boot/main.c
+++ boot/main.c
@@ -1,8 +1,15 @@
 #include "uart.h"
+#include "print.h"
 
 void KMain(void)
 {
-    write_string("Hello, raspberry pi\r\n");
+    uint64_t value = 0x1234567890ABCDEF;
+
+    init_uart();
+    //write_string("Hello, raspberry pi\r\n");
+    printk("Hello, Chris Arm OS\r\n");
+    printk("Test number %d\r\n", value);
+    printk("Test number %x\r\n", value);
     while(1) {
         ;
     }
--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ aarch64-linux-gnu-gcc -c boot/boot.s -o build/boot.o
 aarch64-linux-gnu-gcc -c boot/lib.s -o build/liba.o
 aarch64-linux-gnu-gcc -std=c99 -ffreestanding -mgeneral-regs-only -c boot/main.c -o build/main.o
 aarch64-linux-gnu-gcc -std=c99 -ffreestanding -mgeneral-regs-only -c boot/uart.c -o build/uart.o
-aarch64-linux-gnu-ld -nostdlib -T boot/link.lds -o build/kernel build/boot.o build/main.o build/liba.o build/uart.o
+aarch64-linux-gnu-gcc -std=c99 -ffreestanding -mgeneral-regs-only -c boot/print.c -o build/print.o
+aarch64-linux-gnu-ld -nostdlib -T boot/link.lds -o build/kernel build/boot.o build/main.o build/liba.o build/uart.o build/print.o
 aarch64-linux-gnu-objcopy -O binary build/kernel build/kernel8.img
