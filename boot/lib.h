e683603a6d778ce62e5c444a0118eead97def035
Christian Rafael <christianparadix@gmail.com>
2026-04-03 23:59:22 -0300

    feat: Sending Message On Raspberry PI 3 & qemu (UART)

--- boot/lib.h
+++ boot/lib.h
@@ -3,7 +3,7 @@
 
 #include "stdint.h"
 
-void delay(uint64_t value)
+void delay(uint64_t value);
 void out_word(uint64_t addr, uint32_t value);
 uint32_t in_word(uint64_t addr);
 
--- boot/main.c
+++ boot/main.c
@@ -1,5 +1,8 @@
+#include "uart.h"
+
 void KMain(void)
 {
+    write_string("Hello, raspberry pi\r\n");
     while(1) {
         ;
     }
