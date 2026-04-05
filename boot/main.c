14a78ed8bf26c428192656c65c6237c9a0249b36
Christian Rafael <christianparadix@gmail.com>
2026-04-05 03:18:30 -0300

    feat: Current Exception Level

--- boot/main.c
+++ boot/main.c
@@ -1,6 +1,7 @@
 #include "uart.h"
 #include "print.h"
 #include "debug.h"
+#include "lib.h"
 
 void KMain(void)
 {
@@ -9,6 +10,7 @@ void KMain(void)
     init_uart();
     //write_string("Hello, raspberry pi\r\n");
     printk("Hello, Chris Arm OS\r\n");
+    printk("We are at EL %u\r\n", (uint64_t)get_el());
     ASSERT(0);
     // printk("Test number %d\r\n", value);
     // printk("Test number %x\r\n", value);
